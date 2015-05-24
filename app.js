186b1e7f730771f3042a112d1f0adba5ef57ccc3
Romain Quilliot <romain.addweb@gmail.com>
2015-05-24 02:24:27 +0200

    update appJS

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var io = require('socket.io');
  * Config the app
  */
 var controllers = {
-    join: require('./src/controllers/join.js')
+    channel: require('./src/controllers/channel.js'),
+    message: require('./src/controllers/message.js')
 };
 global.models {
     channels: require('./src/models/channels.js')
@@ -18,8 +19,8 @@ global.models {
  * Application events
  */
 io.on('connection', function (socket) {
-
-    socket.on('channel:join', controllers.join);
+    socket.on('channel:join', controllers.channel.join);
+    socket.on('message:add', controllers.message.add);
 });
 
 
