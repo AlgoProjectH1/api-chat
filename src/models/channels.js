45a92bb9c2abb17a69981948921563f58935154a
Romain Quilliot <romain.addweb@gmail.com>
2015-05-24 02:06:08 +0200

    Add comments

--- src/models/channels.js
+++ src/models/channels.js
@@ -2,20 +2,38 @@ var Channels = {
     channels: {}
 };
 
+/**
+ * Add a channel
+ * @param string channel
+ * @param function callback
+ */
 Channels.add = function (channel, callback) {
     this.channels[channel] = [];
 
     if (callback) callback();
 };
 
+/**
+ * Add an user to a channel
+ * @param string channel
+ * @param object infos
+ */
 Channels.addUser = function (channel, infos) {
     this.channels[channel].push(infos);
 };
 
+/**
+ * Check if a channel exists
+ * @param string channel
+ */
 Channels.exists = function (channel) {
     return (!this.channels[channel]) ? false : true;
 };
 
+/**
+ * Get the users of a channel
+ * @param string channel
+ */
 Channels.get = function (channel) {
     if (channel === undefined) return this.channels;
 
